59f81652d49e417c2255964ef530013ca03739a0
Lukas Kleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-02-09 11:32:02 +0100

    Additional options for webpack

--- webpack.config.hot.js
+++ webpack.config.hot.js
@@ -1,12 +1,16 @@
-var path = require('path');
+var config = require('./gulp.config');
 var webpack = require('webpack');
+var path = require('path');
+
+var task = config.tasks.scripts;
+
 var webpackConfig = require('./webpack.config');
 
 Object.keys(webpackConfig.entry).forEach(key => {
   var entry = webpackConfig.entry[key];
   webpackConfig.entry[key] = [
     'webpack/hot/dev-server',
-    'webpack-hot-middleware/client?reload=true&quiet=true&overlay=true',
+    'webpack-hot-middleware/client?reload=true&quiet=' + task.plugins.webpack.quiet.toString() + '&overlay=' + task.plugins.webpack.overlay.toString(),
   ];
   webpackConfig.entry[key].push(entry);
 });
