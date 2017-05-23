fadfb4bf6bb642e4b0a34e81d58a7bdd3948ba64
lukaskleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-05-23 10:40:10 +0200

    Updated dependencies

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,1 +1,40 @@
-webpackJsonp([0],[function(e,r,t){"use strict";function n(e){return e*e}function o(e,r){return u(n(e)+n(r))}Object.defineProperty(r,"__esModule",{value:!0}),r.square=n,r.diag=o;var u=r.sqrt=Math.sqrt},function(e,r,t){"use strict";var n=t(0),o=function(e){if(e&&e.__esModule)return e;var r={};if(null!=e)for(var t in e)Object.prototype.hasOwnProperty.call(e,t)&&(r[t]=e[t]);return r.default=e,r}(n);console.log(o.square(11)),console.log(o.diag(4,3))}],[1]);
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.square = square;
+exports.diag = diag;
+var sqrt = exports.sqrt = Math.sqrt;
+
+function square(x) {
+  return x * x;
+}
+
+function diag(x, y) {
+  return sqrt(square(x) + square(y));
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _lib = __webpack_require__(0);
+
+var lib = _interopRequireWildcard(_lib);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+console.log(lib.square(11)); // 121
+console.log(lib.diag(4, 3)); // 5
+
+/***/ })
+],[1]);
